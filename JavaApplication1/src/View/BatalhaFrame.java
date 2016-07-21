6ee8d73755cfaa5b249de12498bca50df2c43d82
pauloht <darktiger.p@gmail.com>
2016-07-21 18:00:08 -0300

    Coisas
    CoisasDaView

---
+++ JavaApplication1/src/View/BatalhaFrame.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import CriaturasPackage.BaseCreature;
+import CriaturasPackage.Monstro;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author FREE
+ */
+public class BatalhaFrame extends JFrame{
+    List< BaseCreature > lista_de_criaturas = new ArrayList<>();
+    public BatalhaFrame() throws IOException {
+        
+        //25 + 200 + 25 + 200 + 25 + 200 + 25 + 200 + 25 = 25*5 + 200*4 = 125+800 = 925 = tamanho horizontal
+        this.setLayout(new GridBagLayout());
+        
+        GridBagConstraints c = new GridBagConstraints();
+        
+        JPanel panel1 = new JPanel();
+        panel1.setBackground(Color.BLACK);
+        panel1.setPreferredSize(new Dimension(925,25));
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 37;
+        c.gridheight = 1;
+        this.add(panel1,c);
+        
+        JPanel panel2 = new JPanel();
+        panel2.setBackground(Color.BLACK);
+        panel2.setPreferredSize(new Dimension(25,400));
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 1;
+        c.gridheight = 16;
+        this.add(panel2,c);
+        
+        for (int i = 0;i<2;i++)
+        {
+            BaseCreature creature = new Monstro();
+            lista_de_criaturas.add(creature);
+        }
+        
+        for (int i = 0;i<4;i++)
+        {
+            if (i >= lista_de_criaturas.size())
+            {
+                //System.out.println("label!");
+                JLabel x_label = new JLabel();
+                x_label.setIcon(new ImageIcon( ImageIO.read(new File(getClass().getResource("/View/Imagens/x_icon.png").getFile() ) ) ) );
+                x_label.setPreferredSize(new Dimension(200,400));
+                c.gridx = 1+i*9;
+                c.gridy = 1;
+                c.gridwidth = 8;
+                c.gridheight = 16;
+                this.add(x_label,c);
+            }
+            else
+            {
+                JPanel panel3 = new CartaCriatura(lista_de_criaturas.get(i));
+                panel3.setPreferredSize(new Dimension(200,400));
+                c.gridx = 1+i*9;
+                c.gridy = 1;
+                c.gridwidth = 8;
+                c.gridheight = 16;
+                this.add(panel3,c);
+            }
+            JPanel panel4 = new JPanel();
+            panel4.setBackground(Color.BLACK);
+            panel4.setPreferredSize(new Dimension(25,400));
+            c.gridx = 9+i*9;
+            c.gridy = 1;
+            c.gridwidth = 1;
+            c.gridheight = 16;
+            this.add(panel4,c);
+        }
+        
+        JPanel panel5 = new JPanel();
+        panel5.setBackground(Color.BLACK);
+        panel5.setPreferredSize(new Dimension(925,25));
+        c.gridx = 0;
+        c.gridy = 17;
+        c.gridwidth = 37;
+        c.gridheight = 1;
+        this.add(panel5,c);
+        
+        JPanel panel6 = new JPanel();
+        panel6.setBackground(Color.BLACK);
+        panel6.setPreferredSize(new Dimension(250,25));
+        c.gridx = 0;
+        c.gridy = 18;
+        c.gridwidth = 10;
+        c.gridheight = 1;
+        this.add(panel6,c);
+        
+        JButton btAction = new JButton("Agir");
+        btAction.setPreferredSize(new Dimension(425,25));
+        c.gridx = 10;
+        c.gridy = 18;
+        c.gridwidth = 17;
+        c.gridheight = 1;
+        this.add(btAction,c);
+        
+        JPanel panel7 = new JPanel();
+        panel7.setBackground(Color.BLACK);
+        panel7.setPreferredSize(new Dimension(250,25));
+        c.gridx = 27;
+        c.gridy = 18;
+        c.gridwidth = 10;
+        c.gridheight = 1;
+        this.add(panel7,c);
+        
+        JPanel panel8 = new JPanel();
+        panel8.setBackground(Color.BLACK);
+        panel8.setPreferredSize(new Dimension(925,25));
+        c.gridx = 0;
+        c.gridy = 19;
+        c.gridwidth = 37;
+        c.gridheight = 1;
+        this.add(panel8,c);
+        
+        this.pack();
+        //this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        
+    }
+    
+    public static void main(String args[])
+    {   
+        try {
+            BatalhaFrame frame = new BatalhaFrame();
+        } catch (IOException ex) {
+            Logger.getLogger(BatalhaFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ JavaApplication1/src/View/SeletorCriaturas.java
@@ -0,0 +1,255 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import CriaturasPackage.BaseCreature;
+import CriaturasPackage.Monstro;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author FREE
+ */
+public class SeletorCriaturas extends JFrame{
+    
+    List< BaseCreature > lista;
+    private int ponteiro = 0;
+    private volatile boolean block = false;
+    
+    private final JButton btDireita;
+    private final JButton btEsquerda;
+    private final JButton btAtacar;
+    private final JButton btCancelar;
+    private JPanel panel_criatura;
+    
+    SeletorCriaturas(List< BaseCreature > lista) throws IOException
+    {
+        super();
+        this.lista = lista;
+        
+        setLayout(new GridBagLayout());
+        
+        GridBagConstraints g = new GridBagConstraints();
+        
+        for (int i=0;i<4;i++)
+        {
+            JPanel panel_preenchedor_esquerda = new JPanel();
+            if (i%2==0)
+            {
+                panel_preenchedor_esquerda.setBackground(Color.BLACK);
+            }
+            else
+            {
+                panel_preenchedor_esquerda.setBackground(Color.GRAY);
+            }
+            panel_preenchedor_esquerda.setPreferredSize(new Dimension(200,100));
+            g.gridwidth = 2;
+            g.gridx = 0;
+            g.gridheight = 1;
+            g.gridy = 0 + i;
+            add(panel_preenchedor_esquerda,g);
+        }
+        
+        for (int i=0;i<4;i++)
+        {
+            JPanel panel_preenchedor_direita = new JPanel();
+            if (i%2==0)
+            {
+                panel_preenchedor_direita.setBackground(Color.BLACK);
+            }
+            else
+            {
+                panel_preenchedor_direita.setBackground(Color.GRAY);
+            }
+            panel_preenchedor_direita.setPreferredSize(new Dimension(200,100));
+            g.gridwidth = 2;
+            g.gridx = 4;
+            g.gridheight = 1;
+            g.gridy = 0 + i;
+            add(panel_preenchedor_direita,g);
+        }
+        
+        panel_criatura = new CartaCriatura(lista.get(ponteiro));
+        panel_criatura.setPreferredSize(new Dimension(200,400));
+        g.gridwidth = 2;
+        g.gridx = 2;
+        g.gridheight = 4;
+        g.gridy = 0;
+        add(panel_criatura,g);
+        
+        btEsquerda = new JButton("Esquerda");
+        //carregar imagem seta esquerda
+        btEsquerda.setPreferredSize(new Dimension(200,200));
+        btEsquerda.setIcon( new ImageIcon( ImageIO.read( new File(getClass().getResource("/View/Imagens/arrow_left_icon.png").getFile()) ) ) );
+        btEsquerda.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                try {
+                    btEsquerdaActionPerformed(evt);
+                } catch (IOException ex) {
+                    Logger.getLogger(SeletorCriaturas.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        g.gridwidth = 2;
+        g.gridx = 0;
+        g.gridheight = 2;
+        g.gridy = 4;
+        add(btEsquerda,g);
+        
+        btDireita = new JButton("Direita");
+        //carregar imagem seta direita
+        btDireita.setPreferredSize(new Dimension(200,200));
+        btDireita.setIcon( new ImageIcon( ImageIO.read( new File(getClass().getResource("/View/Imagens/arrow_right_icon.png").getFile()) ) ) );
+        btDireita.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                try {
+                    btDireitaActionPerformed(evt);
+                } catch (IOException ex) {
+                    Logger.getLogger(SeletorCriaturas.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        g.gridwidth = 2;
+        g.gridx = 4;
+        g.gridheight = 2;
+        g.gridy = 4;
+        add(btDireita,g);
+        
+        btAtacar = new JButton("Atacar");
+        btAtacar.setPreferredSize(new Dimension(200,100));
+        btAtacar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btAtacarActionPerformed(evt);
+            }
+        });
+        g.gridwidth = 2;
+        g.gridx = 2;
+        g.gridheight = 1;
+        g.gridy = 4;
+        add(btAtacar,g);
+        
+        btCancelar = new JButton("Cancelar");
+        btCancelar.setPreferredSize(new Dimension(200,100));
+        g.gridheight = 2;
+        g.gridx = 2;
+        g.gridheight = 1;
+        g.gridy = 5;
+        add(btCancelar,g);
+        
+        setResizable(false);
+        checkButtonStatus();
+        pack();
+        setVisible(true);
+    }
+    
+    private void btAtacarActionPerformed(java.awt.event.ActionEvent evt)
+    {
+        
+    }
+    
+    private void btCancelarActionPerformed(java.awt.event.ActionEvent evt)
+    {
+        
+    }
+    
+    private void btEsquerdaActionPerformed(java.awt.event.ActionEvent evt) throws IOException
+    {
+        if (!block)
+        {
+            block = true;
+            ponteiro--;
+            update();
+        }
+    }
+    
+    private void btDireitaActionPerformed(java.awt.event.ActionEvent evt) throws IOException
+    {
+        if (!block)
+        {
+            block = true;
+            ponteiro++;
+            update();
+        }
+    }
+    
+    private void onEnd()
+    {
+        
+    }
+    
+    private void update() throws IOException
+    {
+        panel_criatura = new CartaCriatura(lista.get(ponteiro));
+        checkButtonStatus();
+    }
+    
+    private void checkButtonStatus()
+    {
+        System.out.println("chamado!");
+        if (lista == null)
+        {
+            btDireita.setEnabled(false);
+            btEsquerda.setEnabled(false);
+        }
+        else
+        {
+            if (ponteiro == 0)
+            {
+                btEsquerda.setEnabled(false);
+            }
+            else
+            {
+                btEsquerda.setEnabled(true);
+            }
+            if (ponteiro == lista.size()-1)
+            {
+                btDireita.setEnabled(false);
+            }
+            else
+            {
+                btDireita.setEnabled(true);
+            }
+        }
+        block = false;
+    }
+    
+    public static void main(String args[])
+    {
+        SeletorCriaturas frame;
+        try {
+            List<BaseCreature> lista = new ArrayList<>();
+            lista.add(new Monstro());
+            lista.add(new Monstro());
+            frame = new SeletorCriaturas(lista);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        }
+        catch (Exception e)
+        {
+            System.out.println("erro = " + e.getMessage());
+        }
+    }
+    
+    
+    
+    
+    
+}
