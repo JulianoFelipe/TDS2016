4ff2412b4e4d97fb1d9bde687a021a743706ad04
JulianoFelipe <juliano_lpj@hotmail.com>
2016-07-28 17:02:03 -0300

    Revert "Revert"
    Quase fiz m****.

--- TDS_Game_Juliano_Paulo/src/Model/DAO/HeroiDAO.java
+++ TDS_Game_Juliano_Paulo/src/Model/DAO/HeroiDAO.java
@@ -12,4 +12,12 @@ import Model.Criaturas.Heroi;
  * @author Juliano Felipe da Silva
  */
 public interface HeroiDAO extends GenericDAO<Heroi> {
+    /**
+     * Irá salvar o estado de um herói (apenas 
+     * os dados mais voláteis), e.g. O tanto de
+     * xp e etc.
+     * @param salvar
+     * @return 
+     */
+    boolean salvar (Heroi salvar);
 }
